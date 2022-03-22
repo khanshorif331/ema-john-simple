5e966727eaae7ab1073bb8eaa6ff39fb0cae7aed
khanshorif331 <khanshorif331@gmail.com>
2022-03-23 00:37:09 +0600

    product data loaded from the fake db

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,7 +7,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
