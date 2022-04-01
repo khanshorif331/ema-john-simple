5dad80592a617c9f0e8e0bc8a03dfa5bb08a00d0
khanshorif331 <khanshorif331@gmail.com>
2022-04-02 00:34:22 +0600

    implement review detail section with lot of flex

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -10,7 +10,7 @@ const ReviewItem = (props) => {
             </div>
             <div className='review-item-details-container'>
                 <div className="review-item-details">
-                    <p className='product-name'>{name}</p>
+                    <p className='product-name' title={name}>{name.length >20 ? name.slice(0,20) + '...' : name}</p>
                     <p>Price: <span className='orange-color'>{price}</span></p>
                     <p><small>Shipping: {shipping}</small></p>
                     <p><small>Quantity: {quantity}</small></p>
