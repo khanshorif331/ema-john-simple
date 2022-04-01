2574ac58d2f5aad9aecc1e6c83317f1bbc40af1b
khanshorif331 <khanshorif331@gmail.com>
2022-04-01 21:40:57 +0600

    added some functionality

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+
+const Orders = () => {
+    const [products,setProducts] = useProducts()
+    const [cart,setCart] = useCart(products)
+    return (
+        <div className='shop-container'>
+            <div className='review-items-container'>
+                {
+                    cart.map(product=><ReviewItem 
+                    key={product.id}
+                    product={product}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Orders;
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ReviewItem.css'
+
+const ReviewItem = (props) => {
+    const {name,img ,price,shipping,quantity} = props.product
+    return (
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className='review-item-details-container'>
+                <div className="review-item-details">
+                    <p className='product-name'>{name}</p>
+                    <p>Price: <span className='orange-color'>{price}</span></p>
+                    <p><small>Shipping: {shipping}</small></p>
+                    <p><small>Quantity: {quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
