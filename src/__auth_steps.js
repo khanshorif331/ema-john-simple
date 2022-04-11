8f65b7ce083b3d23c3485863a959c54637f603bc
khanshorif331 <khanshorif331@gmail.com>
2022-04-11 21:22:12 +0600

    adder firebase authentication with email and password

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,10 +1,15 @@
 /******************************
- 1.crete a project in console.firebase.google.com
+ 1.crete a firebase project in console.firebase.google.com
  2.npm install firebase
  3.create firebase.init.js and import getAuth to export auth
  4.firebase setting> authentication > enable email and password auth
  5.create login, signup component , setup route
  6.attach form field handler and form submit handler
+ 7.npm install --save react-firebase-hooks
+ 8.useCreateUserWithEmailAndPassword(auth) from react-firebase-hooks
+ 9.if user is created redirect to the expected page
+ 10.useSignInWithEmailAndPassword(auth)   for signin
+
 
 
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './SignUp.css'
 import googleLogo from '../../images/google-logo-9824.png'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import auth from '../../firebase.init'
 
 const SignUp = () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 	const [confirmPassword, setConfirmPassword] = useState('')
 	const [error, setError] = useState('')
+	const navigate = useNavigate()
+
+	const [createUserWithEmailAndPassword, user] =
+		useCreateUserWithEmailAndPassword(auth)
 
 	const handleEmailBlur = event => {
 		setEmail(event.target.value)
@@ -22,12 +28,20 @@ const SignUp = () => {
 		setError(event.target.value)
 	}
 
+	if (user) {
+		navigate('/shop')
+	}
+
 	const handleCreateUser = event => {
 		event.preventDefault()
 		if (password !== confirmPassword) {
 			setError('Your two password did not match')
 			return
 		}
+		if (password.length < 6) {
+			setError('Password must be 6 caracters or longer')
+		}
+		createUserWithEmailAndPassword(email, password)
 		setError('')
 	}
 
