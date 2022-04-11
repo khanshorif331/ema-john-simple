a67ad69f727974c96242331f62b3204517761f4f
khanshorif331 <khanshorif331@gmail.com>
2022-04-11 21:47:32 +0600

    implemented protected route for proceed order

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { Link } from 'react-router-dom'
+import auth from '../../firebase.init'
 import logo from '../../images/Logo.svg'
 import './Header.css'
 const Header = () => {
+	const [user, setUser] = useAuthState(auth)
 	return (
 		<nav className='header'>
 			<img src={logo} alt='' />
@@ -11,7 +14,7 @@ const Header = () => {
 				<Link to='/orders'>Orders</Link>
 				<Link to='/inventory'>Inventory</Link>
 				<Link to='/about'>About</Link>
-				<Link to='/login'>Login</Link>
+				{user ? <button>Sign Out</button> : <Link to='/login'>Login</Link>}
 			</div>
 		</nav>
 	)
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import './Login.css'
 import googleLogo from '../../images/google-logo-9824.png'
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
@@ -13,6 +13,8 @@ const Login = () => {
 		useSignInWithEmailAndPassword(auth)
 
 	const navigate = useNavigate()
+	const location = useLocation()
+	const from = location.state?.from?.pathname || '/'
 
 	const handleEmailBlur = event => {
 		setEmail(event.target.value)
@@ -22,7 +24,7 @@ const Login = () => {
 	}
 
 	if (user) {
-		navigate('/shop')
+		navigate(from, { replace: true })
 	}
 
 	const handleUserSignIn = event => {
