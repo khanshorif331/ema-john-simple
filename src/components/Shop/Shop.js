14d150107c4705b4ee3d6d68b7828089153d00d0
khanshorif331 <khanshorif331@gmail.com>
2022-04-26 06:22:54 +0600

    added pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,69 +1,105 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import useProducts from '../../hooks/useProducts';
-import { addToDb, getStoredCart } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import useProducts from '../../hooks/useProducts'
+import { addToDb, getStoredCart } from '../../utilities/fakedb'
+import Cart from '../Cart/Cart'
+import Product from '../Product/Product'
 import './Shop.css'
 
 const Shop = () => {
-    const [products,setProducts] = useProducts()
- 
-    const [cart , setCart] = useState([])
+	const [cart, setCart] = useState([])
+	const [pageCount, setPageCount] = useState(0)
+	const [page, setPage] = useState(0)
+	const [size, setSize] = useState(10)
 
-    
+	const [products, setProducts] = useState([])
+	useEffect(() => {
+		fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+			.then(res => res.json())
+			.then(data => setProducts(data))
+	}, [])
 
-    useEffect( ()=>{
-        const storedCart = getStoredCart()
-        const savedCart = []
-        for(const id in storedCart){
-            const addedProduct= products.find(product=>product.id === id)
-            if(addedProduct){
-                const quantity = storedCart[id]
-                addedProduct.quantity = quantity
-                savedCart.push(addedProduct)
-            }
-        }
-        setCart(savedCart)
-    },[products])
+	useEffect(() => {
+		fetch('http://localhost:5000/productCount')
+			.then(res => res.json())
+			.then(data => {
+				const count = data.count
+				const pages = Math.ceil(count / 10)
+				setPageCount(pages)
+			})
+	}, [])
 
-    const handleAddToCart =(selectedProduct)=>{
-        let newCart = []
-        const exists = cart.find(product=>product.id === selectedProduct.id)
-        if(!exists){
+	useEffect(() => {
+		const storedCart = getStoredCart()
+		const savedCart = []
+		for (const id in storedCart) {
+			const addedProduct = products.find(product => product._id === id)
+			if (addedProduct) {
+				const quantity = storedCart[id]
+				addedProduct.quantity = quantity
+				savedCart.push(addedProduct)
+			}
+		}
+		setCart(savedCart)
+	}, [products])
 
-            selectedProduct.quantity=1
-            newCart = [...cart, selectedProduct]
-        }
-        else{
-            const rest = cart.filter(product=>product.id !== selectedProduct.id)
-            exists.quantity = exists.quantity + 1
-            newCart = [...rest,exists]
-        }
-        
-        setCart(newCart)
-        addToDb(selectedProduct.id)
-    }
+	const handleAddToCart = selectedProduct => {
+		let newCart = []
+		const exists = cart.find(product => product._id === selectedProduct._id)
+		if (!exists) {
+			selectedProduct.quantity = 1
+			newCart = [...cart, selectedProduct]
+		} else {
+			const rest = cart.filter(
+				product => product._id !== selectedProduct._id
+			)
+			exists.quantity = exists.quantity + 1
+			newCart = [...rest, exists]
+		}
 
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product=><Product key={product.id}
-                    product={product}
-                    handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}>
-                    <Link to='/orders'>
-                        <button>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
-        </div>
-    );
-};
+		setCart(newCart)
+		addToDb(selectedProduct._id)
+	}
 
-export default Shop;
+	return (
+		<div className='shop-container'>
+			<div className='products-container'>
+				{products.map(product => (
+					<Product
+						key={product._id}
+						product={product}
+						handleAddToCart={handleAddToCart}
+					></Product>
+				))}
+				<div className='pagination'>
+					{[...Array(pageCount).keys()].map(number => (
+						<button
+							className={page === number ? 'selected' : ''}
+							onClick={() => setPage(number)}
+						>
+							{number + 1}
+						</button>
+					))}
+
+					<select onChange={e => setSize(e.target.value)}>
+						<option value='5'>5</option>
+						<option value='10' selected>
+							10
+						</option>
+						<option value='15'>15</option>
+						<option value='15'>20</option>
+					</select>
+				</div>
+			</div>
+			<div className='cart-container'>
+				<Cart cart={cart}>
+					<Link to='/orders'>
+						<button>Review Order</button>
+					</Link>
+				</Cart>
+			</div>
+		</div>
+	)
+}
+
+export default Shop
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-const useProducts = () =>{
-    const [products,setProducts] = useState([])
-    useEffect(()=>{
-        fetch('products.json')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
+const useProducts = () => {
+	const [products, setProducts] = useState([])
+	useEffect(() => {
+		fetch('http://localhost:5000/product')
+			.then(res => res.json())
+			.then(data => setProducts(data))
+	}, [])
 
-    return [products, setProducts]
+	return [products, setProducts]
 }
 
 export default useProducts
-
